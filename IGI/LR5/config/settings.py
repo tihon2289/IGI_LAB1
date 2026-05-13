a5fd8bfd5b10a0847e3a742878bb4728e88feea5
Letkovski_Tihon <tletkovskij@gmail.com>
2026-05-14 00:12:32 +0300

    Fix admin styles with whitenoise

--- IGI/LR5/config/settings.py
+++ IGI/LR5/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,3 +163,4 @@ LOGGING = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
