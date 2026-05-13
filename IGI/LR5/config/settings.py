e72f462ef59a5c477a0a726eac0c748637b79a8e
Letkovski_Tihon <tletkovskij@gmail.com>
2026-05-14 00:25:20 +0300

    Fix static files with new Django storages

--- IGI/LR5/config/settings.py
+++ IGI/LR5/config/settings.py
@@ -163,4 +163,11 @@ LOGGING = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
